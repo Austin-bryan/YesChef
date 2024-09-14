669705d186ae8a061bac321d01429176bd46b606
Austin Bryan <w.austin.bryan@gmail.com>
2024-09-14 17:44:17 -0400

    Added image place holders

--- app/src/main/java/com/example/yeschef/fragments/AddFragment.java
+++ app/src/main/java/com/example/yeschef/fragments/AddFragment.java
@@ -10,10 +10,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.FrameLayout;
+import android.widget.GridLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.yeschef.R;
