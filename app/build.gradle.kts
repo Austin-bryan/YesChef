a4533b0164b441284ded3b6f3ce685e2922d40fa
Prithvi Ghale <prithvighale01@gmail.com>
2024-11-18 15:57:38 -0500

    Fix: Adjusted width of title box in item_list.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application) apply true
-
 }
 
 android {
@@ -28,40 +27,38 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    buildFeatures {
+        viewBinding = true
     }
 }
 
 dependencies {
-    implementation ("androidx.recyclerview:recyclerview:1.2.1")
-    implementation("androidx.cardview:cardview:1.0.0") // Latest version for CardView
-        implementation ("androidx.navigation:navigation-fragment-ktx:2.7.0")
-        implementation ("androidx.navigation:navigation-ui-ktx:2.7.0")
+    // AndroidX Core libraries
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.gridlayout:gridlayout:1.0.0")
 
+    // Navigation Component
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.0")
 
+    // ViewPager2
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
 
-    // Using the libraries from your version catalog
+    // GSON
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    // Using Version Catalog for other dependencies
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.navigation.fragment)
-    implementation(libs.navigation.ui)
-
-    // ViewPager2 dependency
-    implementation("androidx.viewpager2:viewpager2:1.0.0")
 
     // Testing libraries
-    implementation("androidx.viewpager2:viewpager2:1.0.0") // Add ViewPager2 dependency here
-
-    // Add GridLayout dependency
-    implementation("androidx.gridlayout:gridlayout:1.0.0")
-
-    // GSON DEPENDENCY
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("com.google.code.gson:gson:2.10.1")
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
